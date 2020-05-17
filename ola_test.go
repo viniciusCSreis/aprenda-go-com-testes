2e26edf92407e3d692babcac1d372c52a375c005
viniciuscsreis <viniciuscsreis@gmail.com>
2020-05-17 18:58:09 -0300

    default message on ola function

--- ola_test.go
+++ ola_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Vinicius")
-	esperando := "Olá, Vinicius"
-	if resultado != esperando {
-		t.Errorf("resultado '%s', esperando '%s'", resultado, esperando)
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Vinicius")
+		esperando := "Olá, Vinicius"
+		verificaMensagemCorreta(t, resultado, esperando)
+	})
+
+	t.Run("diz 'olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperando := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperando)
+	})
+
 }
