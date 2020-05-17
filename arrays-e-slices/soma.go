c93b90d20252585e41bab06277e5c336ada9980d
viniciuscsreis <viniciuscsreis@gmail.com>
2020-05-17 20:14:25 -0300

    arrays e slices

--- arrays-e-slices/soma.go
+++ arrays-e-slices/soma.go
@@ -2,8 +2,30 @@ package main
 
 func Soma(dados []int) int {
 	soma := 0
-	for _, numero := range dados{
+	for _, numero := range dados {
 		soma += numero
 	}
 	return soma
+}
+
+func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
+	for _, numeros := range numerosParaSomar {
+		somas = append(somas, Soma(numeros))
+	}
+
+	return
+}
+
+func SomaTodoOResto(numerosParaSomar ...[]int) (somas []int) {
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+
+	return somas
+
 }
--- arrays-e-slices/soma_test.go
+++ arrays-e-slices/soma_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
 	t.Run("coleção de qualquer tamanho", func(t *testing.T) {
@@ -12,4 +15,38 @@ func TestSoma(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSomaTudo(t *testing.T) {
+
+	resultado := SomaTudo([]int{1, 2}, []int{0, 9})
+	esperado := []int{3, 9}
+
+	if !reflect.DeepEqual(resultado, esperado) {
+		t.Errorf("resultado %v esperado %v", resultado, esperado)
+	}
+}
+
+func TestSomaTodoOResto(t *testing.T) {
+
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
+	}
+
+	t.Run("faz as somas de alguns slices", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+
+		verificaSomas(t, resultado, esperado)
+	})
+
+	t.Run("soma slices vazios de forma segura", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+		esperado := []int{0, 9}
+
+		verificaSomas(t, resultado, esperado)
+	})
 }
