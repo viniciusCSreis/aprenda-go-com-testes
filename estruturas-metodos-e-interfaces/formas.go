741f4953895c8d5c217e82597eebc71b906769f6
viniciuscsreis <viniciuscsreis@gmail.com>
2020-05-17 21:53:31 -0300

    estruturas-metodos-e-interfaces

--- estruturas-metodos-e-interfaces/formas.go
+++ estruturas-metodos-e-interfaces/formas.go
@@ -1,1 +1,37 @@
 package estruturas_metodos_e_interfaces
+
+import "math"
+
+type Forma interface {
+	Area() float64
+}
+
+type Retangulo struct {
+	Largura float64
+	Altura  float64
+}
+func (r Retangulo) Area() float64 {
+	return r.Largura * r.Altura
+}
+
+func Perimetro(retangulo Retangulo) float64 {
+	return 2 * (retangulo.Largura + retangulo.Altura)
+}
+
+type Circulo struct {
+	Raio float64
+}
+
+func (c Circulo) Area() float64 {
+	return math.Pi * c.Raio * c.Raio
+}
+
+type Triangulo struct {
+	Base   float64
+	Altura float64
+}
+
+func (t Triangulo) Area() float64{
+	return t.Altura * t.Base / 2
+}
+
--- estruturas-metodos-e-interfaces/formas_test.go
+++ estruturas-metodos-e-interfaces/formas_test.go
@@ -1,1 +1,35 @@
 package estruturas_metodos_e_interfaces
+
+import "testing"
+
+func TestPerimetro(t *testing.T) {
+	retangulo := Retangulo{10.0, 10.0}
+	resultado := Perimetro(retangulo)
+	esperando := 40.0
+
+	if resultado != esperando {
+		t.Errorf("Resultado %.2f esperando %.2f", resultado, esperando)
+	}
+}
+
+func TestArea(t *testing.T) {
+	testesArea := []struct {
+		nome    string
+		forma   Forma
+		temArea float64
+	}{
+		{nome: "Retângulo", forma: Retangulo{Largura: 12, Altura: 6}, temArea: 72.0},
+		{nome: "Círculo", forma: Circulo{Raio: 10}, temArea: 314.1592653589793},
+		{nome: "Triângulo", forma: Triangulo{Base: 12, Altura: 6}, temArea: 36.0},
+	}
+
+	for _, tt := range testesArea {
+		t.Run(tt.nome, func(t *testing.T) {
+			resultado := tt.forma.Area()
+			if resultado != tt.temArea {
+				t.Errorf("%#v resultado %.2f, temArea %.2f", tt.forma, resultado, tt.temArea)
+			}
+		})
+
+	}
+}
